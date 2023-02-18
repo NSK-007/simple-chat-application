6b247d1864b409f91b189d691b1329d9dcd89ca6
shiva-kumar-nadigoti <nshiva900@gmail.com>
2023-02-18 20:08:38 +0530

    Finished chat app

--- routes/chat.js
+++ routes/chat.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
+// const LocalStorage = require('node-localstorage').LocalStorage 
 const fs = require('fs');
+// let localStorage = new LocalStorage('./scratch');
+// let username = localStorage.getItem('username')
 
 router.post('/', (req, res, next) => {
     let chat = JSON.parse(JSON.stringify(req.body))
     console.log(chat)
-    // fs.appendFileSync('message.txt')
+    if(chat.username!=undefined && chat.message!=undefined)
+        fs.appendFileSync('message.txt', chat.username+":"+chat.message+"||");
+    res.redirect('/');
 })
+
 
 router.get('/', (req, res, next) => {
-    let chat = 'No Chat exists';
-    chat = fs.readFileSync('message.txt');
-    console.log(chat);
-    res.send(`<html><div>${chat}</div><form action="/" method="POST"><input type="text" name="message" required><button type="submit">send</button></form></html>`)
+    let chat = fs.readFileSync('message.txt', 'utf8');
+    console.log('get msg'+chat)
+    if(chat==='')
+        chat = 'No chat exists'
+    res.send(`<html><div>${chat}</div><form action="/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+    <input type="text" id="message" name="message" required> 
+    <input name="username" type="hidden"  id="username"> 
+    <button type="submit">send</button></form></html>`)
 })
-
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const LocalStorage = require('node-localstorage').LocalStorage 
 router.get('/login', (req, res, next) => {
-    res.send(`<html><form action="/username" method="POST"><input type="text" name="username" required><button type="submit">submit</button></form></html>`)
+    res.send('<form action="/" onsubmit="localStorage.setItem(`username`, document.getElementById(`username`).value)" method="POST"><input id="username" type="text" name"title"><button type="submit">add User</button></form>')
+    console.log(req.body.username);
 })
 
-router.post('/username', (req, res, next) => {
-    console.log(JSON.parse(JSON.stringify(req.body)));
-    let user = JSON.parse(JSON.stringify(req.body))
-    console.log(user.username)
-    let localStorage = new LocalStorage('./scratch');
-    localStorage.setItem('username', user.username)
-    console.log(localStorage.getItem('username'))
-    res.redirect('/');
-})
+
 
 module.exports = router;
